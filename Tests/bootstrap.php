3894dea8d6dde8260104c1a1fc0825215147a479
Bastien Rambure <bastien.rambure@openclassrooms.com>
2015-03-11 10:58:58 +0100

    add boostrap test

---
+++ Tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OpenClassrooms\Tests;
+
+use Composer\Autoload\ClassLoader;
+
+error_reporting(E_ALL | E_STRICT);
+
+/** @var ClassLoader $loader */
+$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader->add('OpenClassrooms\\Tests\\', __DIR__);
