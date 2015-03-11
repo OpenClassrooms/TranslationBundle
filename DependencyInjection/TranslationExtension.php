78ab055005d29c00fd9194569f7137d42efe7383
Bastien Rambure <bastien.rambure@openclassrooms.com>
2015-03-11 17:09:21 +0100

    change namespace

--- DependencyInjection/TranslationExtension.php
+++ DependencyInjection/TranslationExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DependencyInjection;
+namespace OpenClassrooms\Bundle\TranslationBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
