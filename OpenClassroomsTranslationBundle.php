55550dc27afa8205ef06543dea7a94a90e5f8597
Bastien Rambure <bastien.rambure@openclassrooms.com>
2015-03-11 16:36:21 +0100

    add bundle file

---
+++ OpenClassroomsTranslationBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OpenClassrooms\Bundle\TranslationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Bastien Rambure <bastien.rambure@openclassrooms.com>
+ */
+class OpenClassroomsTranslationBundle extends Bundle
+{
+    
+}
