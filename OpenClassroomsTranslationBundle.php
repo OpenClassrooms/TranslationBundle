616ef064db3e75d3c6b5aeed33699403c5d88118
Bastien Rambure <bastien.rambure@openclassrooms.com>
2015-03-11 17:01:47 +0100

    fix extension

--- OpenClassroomsTranslationBundle.php
+++ OpenClassroomsTranslationBundle.php
@@ -2,6 +2,8 @@
 
 namespace OpenClassrooms\Bundle\TranslationBundle;
 
+use DependencyInjection\TranslationExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -9,5 +11,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class OpenClassroomsTranslationBundle extends Bundle
 {
-    
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->registerExtension(new TranslationExtension());
+    }
+
 }
